01c6040b38e471931129d26c3c4ec2ad0139cdef
Ivan <yurkin.ivan5456@gmail.com>
2021-07-25 16:16:11 +0500

    Init

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App, store } from '@feature/app'
+import { Provider } from 'react-redux'
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
+)
